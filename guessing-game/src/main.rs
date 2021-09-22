3377ed1e86a9a6fa4f53d31fc164481a23f0cde7
0xedb <edohbruno@gmail.com>
2021-09-22 22:45:02 +0000

    functions

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -1,8 +1,22 @@
-use rand::Rng;
-use std::cmp::Ordering;
-use std::io;
-
 fn main() {
-    let myNumber = 200;
-    println!("{}", myNumber);
+    let info: (&str, u32, usize, f64) = ("bruno", 100, 0xedb, 302.32);
+    let array = [0xedb; 10];
+
+    println!("{} {} {} {}", info.0, info.2, info.1, info.3);
+    println!("{}\n{}", array[3], array.len());
+    echo(30, 3.0);
+    println!("{}", ten());
+    println!("{}", hundered());
+}
+
+fn echo(x: i32, y: f64) {
+    println!("HELLO {}--{}", x, y);
+}
+
+fn ten() -> i64 {
+    10
+}
+
+fn hundered() -> i128 {
+    100
 }
